cd62746d718d965f5259406c8919e78e7d17424e
Joshua Rose <joshuarose@gmx.com>
2024-10-25 09:00:13 +1100

    feat: added 'new' command
    Added a 'new' command to add a new task. The command has one mandatory
    flag which is --name <task-name> and one optional flag which is --due
    <due-date> . The day must be in a set format - i.e. YYYY-MM-DD and the
    date cannot be in the past, as a check is done to make sure that that is
    not the case.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
+use std::fmt::Display;
+
+use chrono::format::ParseError;
+use chrono::{Local, NaiveDate};
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    New {
+        #[arg(long, value_name = "task-name")]
+        name: String,
+        #[arg(long, value_name = "due-date")]
+        due: Option<String>,
+    },
+}
+
+fn date_from_str(date: &str) -> Result<NaiveDate, ParseError> {
+    NaiveDate::parse_from_str(date, "%Y-%m-%d")
+}
+
+struct Task {
+    name: String,
+    due: Option<NaiveDate>,
+}
+
+impl Display for Task {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.due {
+            Some(value) => write!(f, "Task: {} (due {})", self.name, value),
+            None => write!(f, "Task: {}", self.name),
+        }
+    }
+}
+
+impl Task {
+    fn positive_date_delta(date: NaiveDate) -> bool {
+        Local::now().date_naive() < date
+    }
+
+    fn new(name: String, due: Option<NaiveDate>) -> Self {
+        if due.is_some_and(|x| !Self::positive_date_delta(x)) {
+            eprintln!("Task date must be positive")
+        }
+        Self { name, due }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let parsed_args = Args::parse();
+    let mut tasks: Vec<Task> = Vec::new();
+    match parsed_args.command {
+        Commands::New { name, due } => match due {
+            Some(value) => match date_from_str(&value) {
+                Ok(x) => tasks.push(Task::new(name, Some(x))),
+                Err(_) => {
+                    eprintln!("Date format is YYYY-MM-DD")
+                }
+            },
+            None => {}
+        },
+    }
+
+    if tasks.len() > 0 {
+        for task in tasks.iter() {
+            print!("Added task: {}", task)
+        }
+    }
 }
